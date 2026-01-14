2a9afa5f72868f208644f7dfdcde7b97ee8426a9
Sachin Samuel Sanjay <sachin.sanjay@icloud.com>
2026-01-14 21:34:26 +0400

    fixed

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ use {
 };
 fn main() {
     let ledger_path=Path::new("/mnt/ledger");
-    let admin_client= match admin_rpc_service::connect(ledger_path)?;
+    let admin_client= match admin_rpc_service::connect(ledger_path).await{
+        Ok(client)=>client,
+        Err(e)=>{
+            eprintln!("failed to connect to ledger: {}",e);
+            return;
+        }
+    };  
     println!("connected to ledger");
 }
